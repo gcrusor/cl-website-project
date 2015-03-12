00f6dd7ee5216ee67381c6278904a14c13b69260
Grant Crusor <mixnwell@yahoo.com>
2015-03-12 18:50:47 -0400

    Second Major Commit

--- CL script.js
+++ CL script.js
@@ -1,1 +1,14 @@
-console.log("Hello from myscript.js");
+// My Main CL Website JavaScript
+
+$(".scroll").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     });
